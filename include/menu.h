d904cef2edf609e0c4a99c487fea1eb874440bb7
Luis Boden <luis.boden@gmail.com>
2025-02-02 14:51:22 +0100

    add menu system

--- include/menu.h
+++ include/menu.h
@@ -1,19 +1,29 @@
 #ifndef MENU
 #define MENU
-
 #include "game.h"
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include "util.h"
 
+// ---- CONSTANTS ----
+extern const int UI_ELEMENT_SLOT_SIZE;
+
+// -------- UI-MENU-SYSTEM --------
+// Determines if ui elment needs to be rendered or not;
 enum UIRenderMode {
-    OPEN, 
-    CLOSED
+    SHOW, 
+    HIDE
 };
 
+// Determines if logic for ui needs to updated;
 enum UISelectMode {
     ACTIVE,
     INACTIVE
 };
 
 typedef struct {
+    // build menu tile set;
+    SDL_Texture* renderedTilemap;
     Tileset* tileset;
     enum UISelectMode selectMode;
     enum UIRenderMode renderMode;
@@ -22,7 +32,11 @@ typedef struct {
     int w;
     int h;
     // tilemap for menu element;
-    int 
+    DynamicMatrix* tilemap;
 } UIElement ;
+
+typedef struct {
+    UIElement* uiEls[10];
+} MenuState;
 
 #endif
--- src/menu.c
+++ src/menu.c
@@ -1,4 +1,15 @@
+#include "menu.h"
+#include "game.h"
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include "util.h"
 
+// ---- CONSTANTS ----
+// TODO adjust wenn cap size of menu elements is known for 
+// global-UI, ROOMS have theier own LOCAL-UI;
+const int UI_ELEMENT_SLOT_SIZE = 10;
+
+// -------- UI-MENU-SYSTEM --------
 
 void createTextBox() {
 
