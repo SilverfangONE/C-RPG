dedbefa47fabeaa679861736d014e7404d715dfd
Luis Boden <luis.boden@gmail.com>
2025-02-21 17:11:37 +0100

    add init!

--- src/editor/RPGE_EDIT_context.c
+++ src/editor/RPGE_EDIT_context.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
 #include "RPGE_EDIT_context.h"
+#include "log.h"
 
-CONTEXT_EDITOR_RPGE* init_RPG() {
-    return NULL;
-};
+CONTEXT_EDIT* init_EDIT(char* pName) {
+    CONTEXT_EDIT* context = malloc(sizeof(CONTEXT_EDIT));
+    if (context == NULL) return NULL;
+    context->pName = (char *)malloc(sizeof(char) * 20);
+    if (context->pName == NULL) return NULL;
+    if (pName == NULL)
+    {
+        log_error("init_EDIT(): pName is NULL");
+        errno = EINVAL;
+        return NULL;
+    }
+    strncpy(gContext->pName, "C_RPG", sizeof(gContext->pName));
+ 
 
-void destory_CONTEXT_EDITOR_RPGE(CONTEXT_EDITOR_RPGE* pContext) {
+    context->pName = (char *)malloc(sizeof(pName));
+    strncpy(asset->pathIMG, pathIMG, sizeof(asset->pathIMG));
+  
+}; 
+
+void destory_CONTEXT_EDITOR_RPGE(CONTEXT_EDIT* pContext) {
     free(pContext->pName);
     free(pContext);
 }
 
 void destory_VOID_CONTEXT_EDITOR_RPGE(void* pContext) {
-    destory_CONTEXT_EDITOR_RPGE((CONTEXT_EDITOR_RPGE*) pContext);
+    destory_CONTEXT_EDITOR_RPGE((CONTEXT_EDIT*) pContext);
 }
