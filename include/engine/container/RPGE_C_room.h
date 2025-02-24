7c9995fe026db531540718fcebcb3954757665ed
Luis Boden <luis.boden@gmail.com>
2025-02-24 13:28:42 +0100

    add container

---
+++ include/engine/container/RPGE_C_room.h
@@ -0,0 +1,18 @@
+#ifndef RPGE_C_ROOM
+#define RPGE_C_ROOM
+
+#include "RPGE_G_tilemap.h"
+
+typedef struct Room_C_RPGE {
+    int ID;
+    Tilemap_G_RPGE* tilemap;
+} Room_C_RPGE;
+
+int update_Room_C_RPGE(Room_C_RPGE* room);
+int _update_Room_C_RPGE(void* room);
+int render_Room_C_RPGE(SDL_Renderer* renderer, Room_C_RPGE* room);
+int _render_Room_C_RPGE(SDL_Renderer* renderer, void* room);
+
+void destroy_Room_C_RPGE(Room_C_RPGE* room);
+void _destroy_Room_C_RPGE(void* room);
+#endif
